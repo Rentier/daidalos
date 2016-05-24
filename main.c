f3ab66623633e706f271ab5247503fb932b36d50
Jan-Christoph Klie <git@mrklie.com>
2016-05-25 01:14:21 +0200

    Reformat

--- main.c
+++ main.c
@@ -19,40 +19,37 @@ static quadcopter_t quadcopter;
 /*
  *  ======== taskFxn ========
  */
-Void taskFxn(UArg a0, UArg a1)
-{
-	while(1)
-	{
-    System_printf("enter taskFxn()\n");
+Void taskFxn(UArg a0, UArg a1) {
+	while (1) {
+		System_printf("enter taskFxn()\n");
 
-    // micro seconds
-    Task_sleep(1000);
-    toggle_led();
+		// micro seconds
+		Task_sleep(1000);
+		toggle_led();
 
-    System_printf("exit taskFxn()\n");
+		System_printf("exit taskFxn()\n");
 
-    System_flush(); /* force SysMin output to console */
+		System_flush(); /* force SysMin output to console */
 	}
 }
 
 /*
  *  ======== main ========
  */
-Int main()
-{ 
+Int main() {
 	init_led();
-    Task_Handle task;
-    Error_Block eb;
+	Task_Handle task;
+	Error_Block eb;
 
-    System_printf("enter main()\n");
+	System_printf("enter main()\n");
 
-    Error_init(&eb);
-    task = Task_create(taskFxn, NULL, &eb);
-    if (task == NULL) {
-        System_printf("Task_create() failed!\n");
-        BIOS_exit(0);
-    }
+	Error_init(&eb);
+	task = Task_create(taskFxn, NULL, &eb);
+	if (task == NULL) {
+		System_printf("Task_create() failed!\n");
+		BIOS_exit(0);
+	}
 
-    BIOS_start();    /* does not return */
-    return(0);
+	BIOS_start(); /* does not return */
+	return (0);
 }

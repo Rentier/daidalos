5bb5ba7809704f44646c9c7a132c9337f5151e36
Jan-Christoph Klie <git@mrklie.com>
2016-06-14 20:56:51 +0200

    Add remote control foundation

---
+++ include/remotecontrol.h
@@ -0,0 +1,14 @@
+#ifndef RMR_REMOTECONTROL_H_
+#define RMR_REMOTECONTROL_H_
+
+typedef enum {
+	RC_NICK = 0,
+	RC_ROLL = 1,
+	RC_THRUST = 2,
+	RC_YAW = 3,
+	RC_ARM = 4,
+	RC_HEIGHT_CONTROL = 5,
+	RC_FL_MODE = 6
+} rc_channel;
+
+#endif /* RMR_REMOTECONTROL_H_ */

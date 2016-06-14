59dac68cfc961cdb906a09b323065957c36f3ee0
Jan-Christoph Klie <git@mrklie.com>
2016-06-14 21:08:39 +0200

    Add ultrasonic foundation

---
+++ include/ultrasonic.h
@@ -0,0 +1,8 @@
+#ifndef RMR_ULTRASONIC_H_
+#define RMR_ULTRASONIC_H_
+
+void ultrasonic_init();
+
+void ultrasonic_read();
+
+#endif /* RMR_ULTRASONIC_H_ */
---
+++ source/ultrasonic.c
@@ -0,0 +1,9 @@
+#include "ultrasonic.h"
+
+void ultrasonic_init() {
+
+}
+
+void ultrasonic_read() {
+
+}

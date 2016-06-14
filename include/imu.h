278c1bccba271f4fbed99f60ea10a57f30cec37f
Jan-Christoph Klie <git@mrklie.com>
2016-06-14 21:07:03 +0200

    Add IMU foundation

---
+++ include/imu.h
@@ -0,0 +1,8 @@
+#ifndef RMR_IMU_H_
+#define RMR_IMU_H_
+
+void imu_init();
+
+void imu_read();
+
+#endif
---
+++ source/imu.c
@@ -0,0 +1,12 @@
+#include "imu.h"
+
+#include "F28x_Project.h"
+
+void imu_init() {
+	// Initialize SPI
+}
+
+void imu_read() {
+	// SPI Magic
+	// Write into CLA buffer
+}

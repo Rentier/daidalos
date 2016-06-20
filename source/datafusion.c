64ae1be0a7d0f369974dd106147640d5d10d22e3
Jan-Christoph Klie <git@mrklie.com>
2016-06-21 01:14:22 +0200

    Add datafusion

---
+++ source/datafusion.c
@@ -0,0 +1,21 @@
+#include "datafusion.h"
+
+#include <math.h>
+
+#include <xdc/runtime/System.h>
+
+#include "globals.h"
+
+void datafusion_init() {
+
+}
+
+void datafusion_fuse_pose(vec3f raw_gyro, vec3f raw_acc) {
+
+}
+
+void datafusion_fuse_position() {
+
+}
+
+

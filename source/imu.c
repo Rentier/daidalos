8c81dae15595a7bf5edcdb0295a5f5a9abe8c701
Jan-Christoph Klie <git@mrklie.com>
2016-06-20 18:32:37 +0200

    Generate dummy IMU data

--- source/imu.c
+++ source/imu.c
@@ -1,12 +1,26 @@
 #include "imu.h"
 
-#include "F28x_Project.h"
+#include <xdc/runtime/System.h>
+
+#include "globals.h"
 
 void imu_init() {
 	// Initialize SPI
+	System_printf("Initializing IMU\n");
+
+	cpu_to_cla.w_gyro.x = 0;
+	cpu_to_cla.w_gyro.y = 0;
+	cpu_to_cla.w_gyro.z = 0;
+
+	cpu_to_cla.a_acc.x = 0;
+	cpu_to_cla.a_acc.y = 0;
+	cpu_to_cla.a_acc.z = 0;
 }
 
 void imu_read() {
 	// SPI Magic
 	// Write into CLA buffer
+
+	cpu_to_cla.a_acc = vec3f_sadd(cpu_to_cla.a_acc, 1);
+	cpu_to_cla.w_gyro = vec3f_sadd(cpu_to_cla.w_gyro, 1);
 }
--- source/tasks.c
+++ source/tasks.c
@@ -5,21 +5,30 @@
 #include <ti/sysbios/BIOS.h>
 #include <ti/sysbios/knl/Semaphore.h>
 
+#include "globals.h"
+#include "imu.h"
 #include "led.h"
 #include "semaphores.h"
+#include "vec3f.h"
 
 void task_flush_func(UArg a0, UArg a1) {
 	System_printf("Starting flush task\n");
 	while (1) {
 		System_flush();
-		Task_sleep(1000);
+		Task_sleep(100);
 	}
 }
 
 void task_imu_func(UArg a0, UArg a1) {
 	System_printf("Starting IMU task\n");
+	imu_init();
+
 	while (1) {
-		System_printf("IMU\n");
+		Semaphore_pend(semaphore_mainloop, BIOS_WAIT_FOREVER);
+		imu_read();
+
+		vec3f_print(cpu_to_cla.w_gyro);
+		vec3f_print(cpu_to_cla.a_acc);
 	}
 }
 
@@ -28,8 +37,8 @@ void task_led_func(UArg a0, UArg a1) {
 	led_init();
 
 	while (1) {
-		Semaphore_pend(semaphore_mainloop, BIOS_WAIT_FOREVER);
 		System_printf("LED toggle\n");
 		led_toggle();
+		Task_sleep(1000);
 	}
 }

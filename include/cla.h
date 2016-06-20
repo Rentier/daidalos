f182983b02dd57c728bf866afa05894dc13baa56
Jan-Christoph Klie <git@mrklie.com>
2016-06-20 18:31:26 +0200

    Add fake CLA struct

---
+++ include/cla.h
@@ -0,0 +1,13 @@
+#ifndef RMR_CLA_H_
+#define RMR_CLA_H_
+
+#include "vec3f.h"
+
+typedef struct {
+	vec3f w_gyro;
+	vec3f a_acc;
+	vec3f x_gps;
+	float z_ultra;
+} cpu_to_cla_ram;
+
+#endif /* RMR_CLA_H_ */

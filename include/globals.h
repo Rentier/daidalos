42937e04028f88d22b05f34a2ca778e966b63e5c
Jan-Christoph Klie <git@mrklie.com>
2016-06-20 18:31:53 +0200

    Add dedicated globals file

---
+++ include/globals.h
@@ -0,0 +1,10 @@
+#ifndef RMR_GLOBALS_H_
+#define RMR_GLOBALS_H_
+
+#include "cla.h"
+#include "quadcopter.h"
+
+extern cpu_to_cla_ram cpu_to_cla;
+extern quadcopter_t quadcopter;
+
+#endif /* RMR_GLOBALS_H_ */
---
+++ source/globals.c
@@ -0,0 +1,6 @@
+#include "globals.h"
+
+cpu_to_cla_ram cpu_to_cla;
+
+quadcopter_t quadcopter;
+

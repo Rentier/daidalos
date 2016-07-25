4c77090e0149be55956c8b5a11ace3100e78d3c1
Tommy_89 <to.schmidt.89@gmail.com>
2016-07-25 13:40:01 +0200

    Update comments and todo notes.

--- source/globals.c
+++ source/globals.c
@@ -1,9 +1,7 @@
-//#define GLOBAL
-
 #include "globals.h"
 
-Imu imu;
+Imu imu;	// Calibration information of the inertail sensors.
 
-quadcopter_t quadcopter;
+quadcopter_t quadcopter;	// Global state vector of the system.
 
-RC_Buffer rc_buffer;
+RC_Buffer rc_buffer;	// Buffer for recieving byte stream from graupner remote control
